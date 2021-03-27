1b3bff8fa32f3354def28c516d34202489d40fd2
Otte, Clark <cotte@butler.edu>
2021-03-26 22:23:07 -0400

    Add files via upload

--- Driver.cpp
+++ Driver.cpp
@@ -11,3 +11,28 @@
 #include "Array.h"
 #include "Stack.h"
 
+bool infixToPostfix(std::string s)
+{
+	Stack_Factory f = new Stack_Factory();
+	Stack<Command> OppStack = new Stack<Command>();
+	Stack<int> NumStack = new Stack<int>();
+	
+	for(int i = 0; i < s.length(); i++)
+	{
+		try
+		{
+			num = std::stoi(s[i]);
+		} 
+		catch (std::invalid_argument)
+		{
+			// Error converting token to a number. Invalid input.
+			return nullptr;
+		}
+	}
+	
+}
+
+/* Dr. Ryan, I apologize for the effort put forth on my driver, I did not manage my time well enough.  
+I anticipated having more time during this week on my trip to work on it, however the trip has been busier 
+than anticipated. I have been trying my best to catch up on work from my previous absences in this class and 
+others and I will work as best I can on this during the following week. - Apologies, Clark */
